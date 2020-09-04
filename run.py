a352fd315ef467544028f89857b7d748c217d3c4
Yishan Tsai <ai-2493@AI-2493de-iMac.local>
2020-09-04 10:07:05 +0800

    debug

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-from app import app
-from app import socketio
+from app import app,socketio
+# from app import socketio
 
 socketio.run(app,port=5000)
 # socketio.run(app, debug=True, port=5000)
